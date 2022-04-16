1625a7791055d385483fe7615ba844581722f867
franneck94 <schaffranek.jan@web.de>
2022-04-16 10:30:59 +0200

    up

---
+++ Chapter5_DNN/Chapter5_2_Regression/linReg.py
@@ -0,0 +1,20 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+
+
+if __name__ == "__main__":
+    dataset = load_diabetes()
+    x = dataset.data
+    y = dataset.target.reshape(-1, 1)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30)
+
+    regr = LinearRegression()
+    regr.fit(x_train, y_train)
+    score = regr.score(x_test, y_test)
+    y_pred = regr.predict(x_test)
+
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"Score: {score}")
+    print(f"MSE: {mse}")
---
+++ Chapter5_DNN/Chapter5_2_Regression/vis.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.datasets import load_diabetes
+
+
+if __name__ == "__main__":
+    dataset = load_diabetes()
+    x = dataset.data
+    y = dataset.target
+
+    print(f"Feature names:\n{dataset.feature_names}")
+    print(f"DESCR:\n{dataset.DESCR}")
+
+    df = pd.DataFrame(x, columns=dataset.feature_names)
+    df["y"] = y
+
+    print(df.head(n=10))
+    print(df.info())
+    print(df.describe())
+
+    df.hist(bins=30, figsize=(15, 15))
+    plt.show()
