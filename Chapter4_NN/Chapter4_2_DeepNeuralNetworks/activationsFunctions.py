836bd9516fc8c426667020ababeef6957c6126fc
franneck94 <schaffranek.jan@web.de>
2022-08-23 15:20:38 +0200

    up

--- Chapter4_NN/Chapter4_2_DeepNeuralNetworks/activationsFunctions.py
+++ Chapter4_NN/Chapter4_2_DeepNeuralNetworks/activationsFunctions.py
@@ -8,37 +8,40 @@ IMG_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 def plot_function(
-    input_signal: np.ndarray, output_signal: np.ndarray, name: str = None
+    input_signal: np.ndarray, output_signal: np.ndarray, name: str = ""
 ) -> None:
     plt.step(input_signal, output_signal)
     plt.xlabel("a")
     plt.ylabel("f(a)")
-    plt.xlim(np.min(input_signal) - 0.2, np.max(input_signal) + 0.2)
-    plt.ylim(np.min(output_signal) - 0.2, np.max(output_signal) + 0.2)
-    if name:
-        plt.title(f"Activation function: {name}")
+    plt.title(f"Activation function: {name}")
     plt.show()
 
 
-if __name__ == "__main__":
+def main() -> None:
     input_signal = np.linspace(start=-10, stop=10, num=1000)
 
     # Step function
     # f(a) = 0, if a <= 0 else 1
-    output_signal = [0 if a <= 0 else 1 for a in input_signal]
+    output_signal = np.array([0 if a <= 0 else 1 for a in input_signal])
     plot_function(input_signal, output_signal, name="step")
 
     # Tanh
     # f(a) = tanh(a) = 2 / (1+e^(-2a)) - 1
-    output_signal = [2 / (1 + np.exp(-2 * a)) - 1 for a in input_signal]
+    output_signal = np.array(
+        [2 / (1 + np.exp(-2 * a)) - 1 for a in input_signal]
+    )
     plot_function(input_signal, output_signal, name="tanh")
 
     # SIGMOID
     # sigmoid(a) = 1 / (1 + e^-a)
-    output_signal = [1 / (1 + np.exp(-a)) for a in input_signal]
+    output_signal = np.array([1 / (1 + np.exp(-a)) for a in input_signal])
     plot_function(input_signal, output_signal, name="sigmoid")
 
     # RELU = Rectified Linear Unit
     # f(a) = max (0, a)
-    output_signal = [max(0, a) for a in input_signal]
+    output_signal = np.array([max(0, a) for a in input_signal])
     plot_function(input_signal, output_signal, name="relu")
+
+
+if __name__ == "__main__":
+    main()
--- Chapter4_NN/Chapter4_2_DeepNeuralNetworks/biasNeuron.py
+++ Chapter4_NN/Chapter4_2_DeepNeuralNetworks/biasNeuron.py
@@ -9,80 +9,46 @@ IMG_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def plot_function(
     input_signal: np.ndarray,
-    output_signal_neg: np.ndarray,
     output_signal: np.ndarray,
-    output_signal_pos: np.ndarray,
-    name: str = None,
+    offset: float = 0.0,
+    name: str = "",
 ) -> None:
-    plt.step(input_signal, output_signal_neg, color="blue")
+    plt.step(input_signal + offset, output_signal, color="blue")
+    plt.step(input_signal - offset, output_signal, color="red")
     plt.step(input_signal, output_signal, color="black")
-    plt.step(input_signal, output_signal_pos, color="red")
     plt.xlabel("a")
     plt.ylabel("f(a)")
-    plt.xlim(np.min(input_signal) - 0.2, np.max(input_signal) + 0.2)
-    plt.ylim(np.min(output_signal) - 0.2, np.max(output_signal) + 0.2)
-    plt.legend(["Bias: -2", "Bias: 0", "Bias: 2"])
-    if name:
-        plt.title(f"Activation function: {name}")
+    plt.legend([f"Bias: -{offset}", "Bias: 0", f"Bias: {offset}"])
+    plt.title(f"Activation function: {name}")
     plt.show()
 
 
-if __name__ == "__main__":
+def main() -> None:
     input_signal = np.linspace(start=-10, stop=10, num=1000)
-    bias = 2
+    offset = 2
 
     # Step function
     # f(a) = 0, if a <= 0 else 1
-    output_signal_neg = [0 if (a - bias) <= 0 else 1 for a in input_signal]
-    output_signal = [0 if a <= 0 else 1 for a in input_signal]
-    output_signal_pos = [0 if (a + bias) <= 0 else 1 for a in input_signal]
-    plot_function(
-        input_signal,
-        output_signal_neg,
-        output_signal,
-        output_signal_pos,
-        name="step",
-    )
+    output_signal = np.array([0 if a <= 0 else 1 for a in input_signal])
+    plot_function(input_signal, output_signal, offset, name="step")
 
     # Tanh
     # f(a) = tanh(a) = 2 / (1+e^(-2a)) - 1
-    output_signal_neg = [
-        2 / (1 + np.exp(-2 * (a - bias))) - 1 for a in input_signal
-    ]
-    output_signal = [2 / (1 + np.exp(-2 * a)) - 1 for a in input_signal]
-    output_signal_pos = [
-        2 / (1 + np.exp(-2 * (a + bias))) - 1 for a in input_signal
-    ]
-    plot_function(
-        input_signal,
-        output_signal_neg,
-        output_signal,
-        output_signal_pos,
-        name="tanh",
+    output_signal = np.array(
+        [2 / (1 + np.exp(-2 * a)) - 1 for a in input_signal]
     )
+    plot_function(input_signal, output_signal, offset, name="tanh")
 
     # SIGMOID
     # sigmoid(a) = 1 / (1 + e^-a)
-    output_signal_neg = [1 / (1 + np.exp(-(a - bias))) for a in input_signal]
-    output_signal = [1 / (1 + np.exp(-a)) for a in input_signal]
-    output_signal_pos = [1 / (1 + np.exp(-(a + bias))) for a in input_signal]
-    plot_function(
-        input_signal,
-        output_signal_neg,
-        output_signal,
-        output_signal_pos,
-        name="sigmoid",
-    )
+    output_signal = np.array([1 / (1 + np.exp(-a)) for a in input_signal])
+    plot_function(input_signal, output_signal, offset, name="sigmoid")
 
     # RELU = Rectified Linear Unit
     # f(a) = max (0, a)
-    output_signal_neg = [max(0, a - bias) for a in input_signal]
-    output_signal = [max(0, a) for a in input_signal]
-    output_signal_pos = [max(0, a + bias) for a in input_signal]
-    plot_function(
-        input_signal,
-        output_signal_neg,
-        output_signal,
-        output_signal_pos,
-        name="relu",
-    )
+    output_signal = np.array([max(0, a) for a in input_signal])
+    plot_function(input_signal, output_signal, offset, name="relu")
+
+
+if __name__ == "__main__":
+    main()
